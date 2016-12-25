c548da43acb27d241504a1c7af17c93f3b6c201e
SidHarder <softwarenavigator@gmail.com>
2016-12-25 13:36:59 -0700

    adding code to implement pull blob store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
 'use strict'
 
-module.exports = class IdbBlobStore {
-  
+module.exports = class LevelBlobStore {
+  constructor () {
+    //not sure what needs to be in here
+    //maybe the stuff to setup the connection to the leveldb store
+  }
+
+  write (key, cb) {
+    cb = cb || (() => {})
+
+    if (!key) {
+      return cb(new Error('Missing key'))
+    }
+    
+  }
+
+  read (key) {
+    if (!key) {
+      return pull.error(new Error('Missing key'))
+    }
+
+    //get the value and return it as a pull
+    //return an error if key is not found
+  }
+
+  exists (key, cb) {
+    cb = cb || (() => {})
+
+    if (!key) {
+      return cb(new Error('Missing key'))
+    }
+
+    //call the callback and return true/false?
+  }
+
+  remove (key, cb) {
+    cb = cb || (() => {})
+
+    if (!key) {
+      return cb(new Error('Missing key'))
+    }
+
+    //delete the key
+    //call the callback with status of delete?
+  }
 }
